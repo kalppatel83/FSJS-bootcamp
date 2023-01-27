1622fbe3a6e5bec1e3b883f125ce26ddcf235a0f
Kalp Patel <kalp_patel@rocketmail.com>
2023-01-27 23:21:40 +0530

    Q-2 declared variable & logging with interpolation

---
+++ 05 Javascript/Question-assignments/02_variableTwo.js
@@ -0,0 +1,15 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+const firstName = "Kalp";
+const lastName = "Patel";
+var maritalStatus = "single";
+var country = "INDIA";
+let age = 21;
+
+console.log(`
+ First name: ${firstName}
+ Last name: ${lastName}
+ marital status: ${maritalStatus}
+ country: ${country}
+ age: ${age}
+`);
