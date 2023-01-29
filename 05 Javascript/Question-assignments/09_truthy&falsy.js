f98497c682755ce1969055e3d50123ae17781fef
Kalp Patel <kalp_patel@rocketmail.com>
2023-01-29 22:38:09 +0530

    solved

--- 05 Javascript/Question-assignments/09_truthy&falsy.js
+++ 05 Javascript/Question-assignments/09_truthy&falsy.js
@@ -5,24 +5,47 @@
 // Falsy values
 // 👇👇Here below is the list of values which are considered as falsy values.👇👇
 
-// false, 0 null, ""(empty string), "undefined", NaN
+// false,-0, 0,0n, null, ""(empty string), "undefined", NaN
 
 // If you have anyone of this value while execting if & else statement so your if block of code will be considered as false
 
-const noIsEven = "";
+const noIsEven = -0n;
 if (noIsEven) {
-  // console.log("no. is even");
+  console.log("It's a true statement");
+} else {
+  console.log("a false statement");
 }
 
-let numberArray = [1, 2, 3, 4, 5];
-
-function sum() {
-  let sum = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    sum = sum + arguments[i];
-  }
-  return sum;
+let userCardInfo = null;
+if (userCardInfo) {
+  console.log("It's a true statement");
+} else {
+  console.log("a false statement");
 }
 
-let result = sum(1, 2, 3);
-console.log(result);
+let input = 0;
+if (input) {
+  console.log("It's a true statement");
+} else {
+  console.log("a false statement");
+}
+
+// So What are truthy values anything other than above mentioned will be truthy values
+
+// Any positive or negative number other than 0 will be considered truthy
+let number = -11111111;
+if (number) {
+  console.log("yup again a true statement");
+}
+
+// Also a string with a value is a truthy
+let codition = "false";
+if (codition) {
+  console.log("yup again a true statement");
+}
+
+// assigned an empty (---object {}---)or (---empty array[]---) also will be considered as true statement
+let coditionTwo = {};
+if (coditionTwo) {
+  console.log("yup again a true statement");
+}
